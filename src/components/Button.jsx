68810bc8830011a09cd751613359c641f22c84fb
Ren-mayday <rencel.dayrit@gmail.com>
2025-12-19 13:24:22 +0100

    added comment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import "./Button.css";
-// props, aceptará un children que será el text y onClick la función (evento) necesario
+// props, aceptará un children que será el text y onClick la función (evento) necesario, className para poder manipular css
 const Button = ({ children, onClick, className }) => {
   return (
     <button onClick={onClick} className={className}>
